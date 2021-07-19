55cd815e2a6e0ea2eef20c72ca5cc40cc8734c07
linuxanselmo <programadorantonio@gmail.com>
2021-07-18 23:32:22 -0300

    finalizado o projeto

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ const form = document.getElementById('form')
 form.addEventListener('submit', (e)=>{
     e.preventDefault();
     let nome = document.getElementById('nome').value;
-    let email = document.getElementById('email').nodeValue;
-    let dados = {nome, email};
+    let email = document.getElementById('email').value;
+    let dados = {nome, email,};
     let convert = JSON.stringify(dados);
     localStorage.setItem('lead', convert);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
 })
